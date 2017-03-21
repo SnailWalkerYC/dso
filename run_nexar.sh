ea12b7a9ed710bfddf00e3a64f1dcf8938a3a2fe
Yang Gao <gy20073@gmail.com>
2017-03-21 13:37:53 -0700

    initial

---
+++ run_nexar.sh
@@ -0,0 +1,6 @@
+build/bin/dso_dataset \
+files=/media/psf/Host/Volumes/Data/Berkeley/code_and_data/code/egomotion/OpenSfM/data/0021f08b-1708-41e1-86d7-90780b35b5cc/images \
+calib=camera_nexar.txt \
+preset=2 \
+mode=1
+
