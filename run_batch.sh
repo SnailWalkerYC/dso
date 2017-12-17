25b46742d9b7501f99f17c0eb1206027b789c0e9
Yang Gao <gy20073@gmail.com>
2017-12-16 18:58:40 -0800

    mv results to the corresponding folder

--- run_batch.sh
+++ run_batch.sh
@@ -1,10 +1,12 @@
 #path="/data/abobu/data_dso/00488b40-0a8cf1a0"
 
-for path in "/data/abobu/data_dso/*"
+for path in /data/abobu/data_dso/*;
 do
+    echo $path
     video=$path"/*.mov"
 
     bash video_to_images.sh $video  $path"/images"
 
     build/bin/dso_dataset files=$path"/images" calib=camera_nexar.txt preset=2 mode=1 nogui=1
+    mv result.txt $path
 done
